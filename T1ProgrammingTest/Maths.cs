3c0a20b0d3cddf0a4228e9b6825c2ea5739204cf
Vinay Kumar Ghagarwale <VGhagarwale@Portal01.nextgen.com>
2019-03-15 20:05:57 +0530

    add new files

---
+++ T1ProgrammingTest/Maths.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T1ProgrammingTest
+{
+    class Maths
+    {
+    }
+}
---
+++ T1ProgrammingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T1ProgrammingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
